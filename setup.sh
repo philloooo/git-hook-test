9771f78483f2b50926844c915a958403e39e586b
philloooo <phillis.tt@gmail.com>
2015-07-15 16:49:55 -0500

    Example setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+# example setup script that can be used for other services repos
 git clone --depth=1 https://github.com/philloooo/git-hook-test.git
 cp git-hook-test/pre-commit .git/hooks/pre-commit
 rm -rf git-hook-test
