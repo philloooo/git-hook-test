191ef1fb43e4409d7569c44af4a07ae1b118057e
philloooo <phillis.tt@gmail.com>
2015-07-15 17:56:15 -0500

    Move pre-commits to sub directory

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 # example setup script that can be used for other services repos
-git clone --depth=1 https://github.com/philloooo/git-hook-test.git
-cp git-hook-test/pre-commit .git/hooks/pre-commit
-rm -rf git-hook-test
+git clone https://github.com/NCI-GDC/git-hooks.git 
+cp -r git-hooks/*commits .git/hooks/
+
+# if you want to run all pre-commit scripts, copy pre-commit to .git/hooks
+cp git-hooks/pre-commit .git/hooks/
+
+rm -rf git-hooks
