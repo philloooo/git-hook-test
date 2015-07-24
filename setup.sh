553c12e5d8c9cd899594e4d54516a8045ffc58e7
philloooo <phillis.tt@gmail.com>
2015-07-24 17:15:36 -0500

    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-# example setup script that can be used for other services repos
-git clone https://github.com/NCI-GDC/git-hooks.git 
-cp -r git-hooks/hooks/* .git/hooks/
+#!/bin/bash
 
+#setup script that can be called from other services repos
+cp --backup=-t git-hooks/hooks/* .git/hooks/ 
 rm -rf git-hooks
