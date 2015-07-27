e0054b713e44e11293281febbf0631cb9966fbdb
philloooo <phillis.tt@gmail.com>
2015-07-27 13:02:42 -0500

    This fixes #3 and #4

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 #setup script that can be called from other services repos
-cp -r --backup=t git-hooks/hooks/* .git/hooks/ 
-rm -rf git-hooks
-echo "Git hooks successfully setup"
+cp -r --backup=t git-hooks/hooks/* .git/hooks/ || cp -r git-hooks/hooks* .git/hooks/
+if [ $? -eq 0 ]
+then
+    echo "Git hooks successfully setup"
+else
+    echo "Git hooks fail to setup"
+fi
+rm -r git-hooks
